5a1e02f87ed3f7ff06baebef2eca1476d9dac436
wzh <568908252@qq.com>
2021-03-18 20:45:28 +0800

    feature

--- src/main/java/com/bzrrr/kbjspider/service/InsPersistService.java
+++ src/main/java/com/bzrrr/kbjspider/service/InsPersistService.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.bzrrr.kbjspider.dao.InsPersistDao;
 import com.bzrrr.kbjspider.dao.InsUserPersistDao;
 import com.bzrrr.kbjspider.model.dto.InsDto;
+import com.bzrrr.kbjspider.model.dto.InsUserDto;
 import com.google.common.collect.Queues;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.*;
@@ -22,23 +24,7 @@ import java.util.concurrent.*;
  */
 @Service
 @Transactional
-public class InsPersistService extends ServiceImpl<InsPersistDao, InsDto> implements Runnable {
-    private LinkedBlockingQueue<InsDto> dtoQueue = new LinkedBlockingQueue<>();
-    private List<InsDto> dataList = new ArrayList<>();
-    private final int PERSIST_SIZE = 100;
-
-    ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
-            .setNameFormat("persist-pool-%d").build();
-    ExecutorService singleThreadPool = new ThreadPoolExecutor(1, 1,
-            0L, TimeUnit.MILLISECONDS,
-            new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
-
-    public ExecutorService getSingleThreadPool() {
-        return singleThreadPool;
-    }
-
-    @Resource
-    private InsPersistDao dao;
+public class InsPersistService extends ServiceImpl<InsPersistDao, InsDto> {
     @Resource
     private InsUserPersistDao insUserPersistDao;
 
@@ -57,50 +43,20 @@ public class InsPersistService extends ServiceImpl<InsPersistDao, InsDto> implem
         for (InsDto dto : list) {
             System.out.println(dto.getUsername() + " -- " + dto.getLink());
         }
-//        if (list.size() > 0) {
-//            this.saveBatch(list);
-//            QueryWrapper<InsUserDto> userWrapper = new QueryWrapper<>();
-//            userWrapper.eq("username", list.get(0).getUsername());
-//            InsUserDto userDto = insUserPersistDao.selectOne(userWrapper);
-//            if (userDto != null) {
-//                userDto.setUpdateTime(new Date());
-//                insUserPersistDao.updateById(userDto);
-//            } else {
-//                userDto = new InsUserDto();
-//                userDto.setUpdateTime(new Date());
-//                userDto.setUsername(list.get(0).getUsername());
-//                insUserPersistDao.insert(userDto);
-//            }
-//        }
-    }
-
-    @Override
-    public void run() {
-        while (true) {
-            try {
-                dataList.clear();
-                Queues.drain(dtoQueue, dataList, PERSIST_SIZE, 60, TimeUnit.SECONDS);
-                saveList(dataList);
-            } catch (InterruptedException e) {
-                log.error(e.getMessage(), e);
+        if (list.size() > 0) {
+            this.saveBatch(list);
+            QueryWrapper<InsUserDto> userWrapper = new QueryWrapper<>();
+            userWrapper.eq("username", list.get(0).getUsername());
+            InsUserDto userDto = insUserPersistDao.selectOne(userWrapper);
+            if (userDto != null) {
+                userDto.setUpdateTime(new Date());
+                insUserPersistDao.updateById(userDto);
+            } else {
+                userDto = new InsUserDto();
+                userDto.setUpdateTime(new Date());
+                userDto.setUsername(list.get(0).getUsername());
+                insUserPersistDao.insert(userDto);
             }
         }
-    }
-
-    public void sendAsync(InsDto dto) {
-        dtoQueue.offer(dto);
-    }
-
-    public void shutdown() {
-        while (!dtoQueue.isEmpty()) {
-            try {
-                dataList.clear();
-                Queues.drain(dtoQueue, dataList, PERSIST_SIZE, 5, TimeUnit.SECONDS);
-                saveList(dataList);
-            } catch (InterruptedException e) {
-                log.error(e.getMessage(), e);
-            }
-        }
-        singleThreadPool.shutdown();
     }
 }
